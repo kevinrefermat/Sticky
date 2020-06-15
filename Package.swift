255bea67b376e90966012ffee1afeec64a4e145c
Kevin Refermat <kevinrefermat@gmail.com>
2020-06-15 11:53:32 -0700

    closes #6 added minimum platform requirement to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Sticky",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3),
+    ],
     products: [
         .library(
             name: "Sticky",
